f92e74ce6cf7687be3ab0ae722b8cfe626838ad6
atharvakamthe0208 <atharvakamthe2006@gmail.com>
2026-08-10 19:09:29 +0530

    chech number is even or odd
    using array

---
+++ evodarray.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int arr[5]={10,20,30,40,50};
+    int i,sum=0,cnt=0,cnt1=0;
+    int evsum=0,odsum=0;
+
+    int a[10];
+
+    printf("Enter Any ten numbers  :");
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0;i<=9;i++)
+    {
+        if(a[i]%2==0)
+        {
+            evsum=evsum+a[i];
+            cnt++;
+
+            //printf("The number %d is even \n",a[i]);
+        }else
+        {
+            odsum=odsum+a[i];
+            cnt1++;
+            //printf("The number %d is odd\n",a[i]);
+        }
+    }
+
+    printf("The Total even numbers are : %d and Sum is %d\n",cnt,evsum);
+    printf("The Total odd numbers are  : %d and Sum is %d\n",cnt1,odsum);
+
+
+
+    return 0;
+
+}
