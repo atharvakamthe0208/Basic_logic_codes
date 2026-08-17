a8fde3c6bef8001418331a5d532268f45a0b243f
atharvakamthe0208 <atharvakamthe2006@gmail.com>
2026-08-17 19:36:42 +0530

    accept the rows and cols from user
    addittion of matrix

--- matrix.c
+++ matrix.c
@@ -1,46 +1,58 @@
 #include <stdio.h>
+#define max 20
 int main()
 {
-    int i,j;
-    int mat1[3][3],mat2[3][3],mat3[3][3];
+    int i,j,r1,r2,c1,c2;
+    int mat1[max][max],mat2[max][max],mat3[max][max];
 
-    printf("Enter elements for matrix 1 :");
-    for(i=0;i<3;i++)
+    printf("Enter no of rows and col in matrix 1 :");
+    scanf("%d%d",&r1,&c1);
+
+    printf("Enter no of rows and col in matrix 2 :");
+    scanf("%d%d",&r2,&c2);
+
+
+
+    if((r1==r2)&&(c1==c2))
     {
-       for(j=0;j<3;j++)
+        printf("Enter elements for matrix 1 :");
+        for(i=0;i<r1;i++)
         {
-           scanf("%d",&mat1[i][j]);
-        }
-    } 
-    
-    printf("Enter elements for matrix 2 :");
-    for(i=0;i<3;i++)
-    {
-       for(j=0;j<3;j++)
-        {
-           scanf("%d",&mat2[i][j]);
-        }
-    } 
-    
-    
-    for(i=0;i<3;i++)
-    {
-       for(j=0;j<3;j++)
-        {
-           mat3[i][j]=mat2[i][j]+mat1[i][j];
-        }
+        for(j=0;j<c1;j++)
+            {
+            scanf("%d",&mat1[i][j]);
+            }
+        } 
         
-    }  
-
-    printf("Addition of 2 matrix :\n");
-    for(i=0;i<3;i++)
-    {
-       for(j=0;j<3;j++)
+        printf("Enter elements for matrix 2 :");
+        for(i=0;i<r2;i++)
         {
-            printf("%5d",mat3[i][j]);
-        }
-        printf("\n");
-    }                
+        for(j=0;j<c2;j++)
+            {
+            scanf("%d",&mat2[i][j]);
+            }
+        } 
+        
+        
+        for(i=0;i<r1;i++)
+        {
+        for(j=0;j<c2;j++)
+            {
+            mat3[i][j]=mat2[i][j]+mat1[i][j];
+            }
+            
+        }  
+
+        printf("Addition of 2 matrix :\n");
+        for(i=0;i<r1;i++)
+        {
+        for(j=0;j<c2;j++)
+            {
+                printf("%5d",mat3[i][j]);
+            }
+            printf("\n");
+        } 
+    }                   
     return 0;
 
 }
