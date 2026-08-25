974bdbf34dd13e6bf68baa5a535bf9a03e1566cf
atharvakamthe0208 <atharvakamthe2006@gmail.com>
2026-08-25 19:38:45 +0530

    multiplication added

--- argfunc.c
+++ argfunc.c
@@ -17,16 +17,28 @@ int sub()
     return s;
 
 }
+int mul(int n1,int n2)
+{
+    int mul;
+    mul=n1*n2;
 
+    return mul;
+}
 int main()
 {
 
-    int n1,n2,s;
+    int n1,n2,s,m;
     printf("Enter two numbers :");
     scanf("%d%d",&n1,&n2);
     add(n1,n2);
 
     s=sub();
     printf("Substraction of two numbers = %d",s);
+
+    printf("Enter two numbers :");
+    scanf("%d%d",&n1,&n2);
+    m=mul(n1,n2);
+    
+    printf("Multiplication of two numbers = %d",m);
     return 0;
 }
