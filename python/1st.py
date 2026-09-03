c5c23c1489b3a9338621fcc75894b1ee8fac3ba3
atharvakamthe0208 <atharvakamthe2006@gmail.com>
2026-09-03 23:07:55 +0530

    1st python

---
+++ python/1st.py
@@ -0,0 +1,5 @@
+a=10;
+b=20;
+c=a+b
+print("Addition of",a,"and",b,"is ",c);
+print(f"Addition of {a}and {b}is {c}")
---
+++ python/intro.py
@@ -0,0 +1,6 @@
+name="Atharva s Kamthe"
+Age=20
+city="pune"
+course="Data Science"
+print("\tINTRODUCTION")
+print(f"Name   : {name}\nAge    : {Age}\nCity   : {city}\nCourse : {course}")
