79fa8b5ea67d9bacaba5784dac707fd3e3424fd4
atharvakamthe0208 <atharvakamthe2006@gmail.com>
2026-09-07 19:00:18 +0530

    python datatypes

---
+++ python/datatype.py
@@ -0,0 +1,25 @@
+age=int(input("Enter your age "))
+print(age,type(age))
+
+name=input("Enter your NAme :")
+print(name,type(name))
+
+marks=float(input("Enter your marks "))
+print(marks,type(marks))
+
+io=bool(input("Enter status "))
+print(io,type(io))
+
+print()
+fav_color={"pink","green","red"}
+print(fav_color,type(fav_color))
+
+price=(100,200)
+print(price,type(price))
+
+s1={
+    id:"101",name:"atharva"
+}
+print(s1,type(s1))
+
+
---
+++ python/dpex1.py
@@ -0,0 +1,15 @@
+a1=int(input("Enter 1st number "))
+sq=a1*a1
+print(a1)
+
+a2=int(input("Enter 2nd number"))
+cube=a2*a2*a2
+
+tot=sq+cube
+
+print("square :",sq)
+print("Cube:",cube)
+print("Sum=",tot)
+print("Sum=",(sq+cube+tot))
+
+
