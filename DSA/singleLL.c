b0961ee9a0c50ad1e6199a035d2e372bb9ddd041
atharvakamthe0208 <atharvakamthe2006@gmail.com>
2026-09-26 12:43:30 +0530

    Update singleLL.c

--- DSA/singleLL.c
+++ DSA/singleLL.c
@@ -1,174 +1,198 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct  node
-{
-    int data;
-    struct node *add;
-};
-
-struct node *head = NULL;
-
-void insertfrombegin(int val)
-{
-    struct node *newnode=malloc(sizeof(struct node));
-    newnode->data=val;
-    newnode->add=head;
-
-    head=newnode;
-}
-
-void display()
-{
-    struct node *temp=head;
-
-    if(head==NULL)
+    #include <stdio.h>
+    #include <stdlib.h>
+    struct  node
     {
-        printf("Linked list is empty ");
-        return;
-    }
+        int data;
+        struct node *add;
+    };
 
-    while (temp!=NULL)
+    struct node *head = NULL;
+
+    void insertfrombegin(int val)
     {
-        printf("%d->\t",temp->data);
-        temp=temp->add;
-    }
-    
+        struct node *newnode=malloc(sizeof(struct node));
+        newnode->data=val;
+        newnode->add=head;
 
-}
-void deletefrombegin()
-{
-    struct node * temp=head;
-
-    if(head==NULL)
-    {
-        printf("Linked list is empty ");
-        return;
-    }
-
-    head=temp->add;
-    free(temp);
-
-}
-void inserttoend(int val)
-{
-    struct  node *newnode=malloc(sizeof(struct node));
-
-    newnode->data=val;
-    newnode->add=NULL;
-
-    if(head==NULL)
-    {
         head=newnode;
     }
-    else
-    {
-        struct  node *temp=head;
-        while (temp->add!=NULL)
-        {
-            temp=temp->add;
-        }
 
-        temp->add=newnode;
-        
-    }
-    
-    
-}
-void search(int val)
-{
-    struct node *temp=head;
-    while (temp!=NULL)//a100!=null
+    void display()
     {
-        if(temp->data==val)
+        struct node *temp=head;
+
+        if(head==NULL)
         {
-            printf("\ndata found");
+            printf("Linked list is empty ");
             return;
         }
-        temp=temp->add;
-            
-    }
-    printf("\ndata not found"); 
-        //temp->add=newnode;
-}
-void deletefromend(int val)
-{
 
-}
-int main()
-{
-    int ch, data;
-    printf("=================Single Linked List Operations=================");
-    printf("\n1.InsertFromBegin ");
-    printf("\n2.InserttoEnd ");
-    printf("\n3.DeleteFromBegin ");
-    printf("\n4.DeletefromEnd ");
-    printf("\n5.Search ");
-    printf("\n6.Display ");
-    printf("\n7.Exit ");
-    do
-    {
-        printf("Enter your choice :");
-        scanf("%d",&ch);
-       switch (ch)
+        while (temp!=NULL)
         {
-            case 1:
-                printf("Enter data: ");
-                scanf("%d", &data);
-                insertfrombegin(data);
-                break;
+            printf("%d->\t",temp->data);
+            temp=temp->add;
+        }
+        
 
-            case 2:
-                printf("Enter data: ");
-                scanf("%d", &data);
-                inserttoend(data);
-                break;
+    }
+    void deletefrombegin()
+    {
+        struct node * temp=head;
 
-            case 3:
-                deletefrombegin();
-                break;
-
-            case 4:
-                //deletefromend();
-                break;
-
-            case 5:
-                printf("Enter element to search: ");
-                scanf("%d", &data);
-                search(data);
-                break;
-
-            case 6:
-                display();
-                break;
-
-            case 7:
-                printf("Exiting program...\n");
-                break;
-
-            default:
-                printf("Invalid Choice!\n");
+        if(head==NULL)
+        {
+            printf("Linked list is empty ");
+            return;
         }
 
-    } while (ch != 7);
+        head=temp->add;
+        free(temp);
 
+    }
+    void inserttoend(int val)
+    {
+        struct  node *newnode=malloc(sizeof(struct node));
 
+        newnode->data=val;
+        newnode->add=NULL;
 
-    
-    return 0;
+        if(head==NULL)
+        {
+            head=newnode;
+        }
+        else
+        {
+            struct  node *temp=head;
+            while (temp->add!=NULL)
+            {
+                temp=temp->add;
+            }
+
+            temp->add=newnode;
+            
+        }
+        
+        
+    }
+    void search(int val)
+    {
+        struct node *temp=head;
+        while (temp!=NULL)//a100!=null
+        {
+            if(temp->data==val)
+            {
+                printf("\ndata found");
+                return;
+            }
+            temp=temp->add;
+                
+        }
+        printf("\ndata not found"); 
+            //temp->add=newnode;
+    }
+    void deletefromend()
+{
+    if(head == NULL)
+    {
+        printf("Linked list is empty");
+        return;
+    }
+
+    // If there is only one node
+    if(head->add == NULL)
+    {
+        free(head);
+        head = NULL;
+        return;
+    }
+
+    struct node *temp = head;
+    struct node *prev = NULL;
+
+    while(temp->add != NULL)
+    {
+        prev = temp;
+        temp = temp->add;
+    }
+
+    prev->add = NULL;
+    free(temp);
 }
+    int main()
+    {
+        int ch, data;
+        printf("=================Single Linked List Operations=================");
+        printf("\n1.InsertFromBegin ");
+        printf("\n2.InserttoEnd ");
+        printf("\n3.DeleteFromBegin ");
+        printf("\n4.DeletefromEnd ");
+        printf("\n5.Search ");
+        printf("\n6.Display ");
+        printf("\n7.Exit ");
+        do
+        {
+            printf("Enter your choice :");
+            scanf("%d",&ch);
+        switch (ch)
+            {
+                case 1:
+                    printf("Enter data: ");
+                    scanf("%d", &data);
+                    insertfrombegin(data);
+                    break;
 
-//inserttoend(10);
-    //inserttoend(20);
-    //inserttoend(30);
-    //inserttoend(40);
-    //display();
-    //search(30);
-    //search(44);
-    
-    //insertfrombegin(10);
-    //insertfrombegin(20);
-    //insertfrombegin(30);
-    //insertfrombegin(40);
-    //deletefrombegin();
-    //printf("\n"); 
-    //display();
+                case 2:
+                    printf("Enter data: ");
+                    scanf("%d", &data);
+                    inserttoend(data);
+                    break;
+
+                case 3:
+                    deletefrombegin();
+                    break;
+
+                case 4:
+                    deletefromend();
+                    break;
+
+                case 5:
+                    printf("Enter element to search: ");
+                    scanf("%d", &data);
+                    search(data);
+                    break;
+
+                case 6:
+                    display();
+                    break;
+
+                case 7:
+                    printf("Exiting program...\n");
+                    break;
+
+                default:
+                    printf("Invalid Choice!\n");
+            }
+
+        } while (ch != 7);
+
+
+
+        
+        return 0;
+    }
+
+    //inserttoend(10);
+        //inserttoend(20);
+        //inserttoend(30);
+        //inserttoend(40);
+        //display();
+        //search(30);
+        //search(44);
+        
+        //insertfrombegin(10);
+        //insertfrombegin(20);
+        //insertfrombegin(30);
+        //insertfrombegin(40);
+        //deletefrombegin();
+        //printf("\n"); 
+        //display();
